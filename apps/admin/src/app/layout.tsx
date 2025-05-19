4ef8fba621a1b5078c0b19ea9392b06533c18785
Diana <dmoore@xn4p4lm.com>
2025-05-19 22:19:20 +0000

    refactor admin and moving everything to the base app router

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -6,6 +6,8 @@ import Footer from "@/components/navigation/footer";
 import Navbar from "@/components/navigation/navbar";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Sidebar from "@/components/navigation/sidebar";
+import { createClient } from "@/utils/supabase/server";
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
@@ -13,16 +15,36 @@ export const metadata: Metadata = {
   description: "Community focused hosting.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabaseClent = await createClient();
+
+  const user = (await supabaseClent.auth.getUser()).data.user;
+
+  if (!user) {
+    console.log("No user found");
+  }
+
   return (
     <html lang="en">
       <body className="">
         <Navbar />
-        <div className="">{children}</div>
+        <div className="flex md:flex-row ">
+          <div className="hidden lg:flex lg:w-60 pt-16 h-screen bg-base-200">
+            <div className="p-2">
+              {Sidebar()}
+            </div>
+
+          </div >
+          <div className="bg-base-100 flex-grow min-h-dvh b-48 pt-16">
+            <div className="p-5">
+              {children}
+            </div>
+          </div>
+        </div >
         <Footer />
       </body>
     </html>
--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,6 +1,25 @@
-import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { prisma } from "database";
 
-export default async function Home() {
+export default async function Dashboard() {
+    const supabase = await createClient();
+    const userObject = await supabase.auth.getUser();
+    const user_id = userObject.data.user?.id;
 
-  redirect("/dashboard");
+    const vm_count = await prisma.services.count({
+        where: {
+            user_id: user_id,
+            subscription_active: true
+        },
+    });
+
+    return (
+        <div>
+            <div>
+                Welcome to the dashboard!
+            </div>
+            <div>
+                You have {vm_count} VMs active.
+            </div>
+        </div>);
 }
