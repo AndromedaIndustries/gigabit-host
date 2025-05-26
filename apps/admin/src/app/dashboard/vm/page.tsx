00b47161f64ab2f4a8c9ab86df6bc240598547a5
Diana <dmoore@xn4p4lm.com>
2025-05-26 07:48:58 +0000

    feat: Add total costs and total VMs display to dashboard, enhance VmCard with SKU details

--- apps/admin/src/app/dashboard/vm/page.tsx
+++ apps/admin/src/app/dashboard/vm/page.tsx
@@ -14,12 +14,37 @@ export default async function VMs() {
             subscription_active: true
         },
     });
+
+    let totalCosts = 0;
+    let totalVms = 0;
+
+    // Calculate total costs for all VMs
+    for (const vm of vms) {
+        totalVms++;
+        const sku = await prisma.sku.findUnique({
+            where: {
+                id: vm.current_sku_id
+            },
+        });
+        if (sku) {
+            totalCosts += sku.price;
+        }
+    }
+
     return (
         <div className="w-full h-fit pb-24">
             <div className="flex flex-row flex-wrap space-x-5 space-y-5">
                 <div className="card card-border bg-base-300 w-64 h-64">
-                    <div className="w-full h-full flex flex-col items-center justify-center">
-                        <Link href={"/dashboard/vm/new"} className="btn btn-outline btn-primary place-self-center">
+                    <div className="card-body items-center text-center">
+                        <div className="card-title text-2xl">Total Costs</div>
+                        <div className="text-lg">
+                            {totalCosts.toFixed(2)} <span className="text-sm">$/mo</span>
+                        </div>
+                        <div className="card-title text-2xl">Total VMs</div>
+                        <div className="text-lg">
+                            {totalVms}
+                        </div>
+                        <Link href={"/dashboard/vm/new"} className="btn btn-outline btn-primary mt-auto">
                             Add new VM
                         </Link>
                     </div>
@@ -27,6 +52,7 @@ export default async function VMs() {
 
                 {vms.length === 0 ? null :
                     vms.map((vm) => (
+                        // Render each VM card
                         <VmCard vm={vm} key={vm.id} />
                     ))}
             </div>
--- apps/admin/src/components/cards/vm.tsx
+++ apps/admin/src/components/cards/vm.tsx
@@ -1,11 +1,23 @@
-import type { Services } from "database";
+import type { Services, Sku } from "database";
 import Link from "next/link";
+import { prisma } from "database";
 
 type vmCardProp = {
     vm: Services
 }
 
-export function VmCard({ vm }: vmCardProp) {
+async function getSku(skuId: string): Promise<Sku | null> {
+
+    const skuName = await prisma.sku.findUnique({
+        where: {
+            id: skuId
+        },
+    });
+
+    return skuName;
+}
+
+export async function VmCard({ vm }: vmCardProp) {
 
     // get the FQDN of the VM
     const fqdn = vm.hostname
@@ -14,14 +26,43 @@ export function VmCard({ vm }: vmCardProp) {
     const url = new URL(`http://${fqdn}`);
     const domain = url.hostname;
 
+    // get the sku from the database using the sku_id from the vm object
+    const sku = await getSku(vm.current_sku_id);
+
     return (
         <div className="card card-border bg-base-300 w-64 h-64" key={vm.id}>
             <div data-sku-id={vm.sku_id} className="card-body items-center text-center">
-                <h2 className="card-title">{domain}</h2>
-                <div className="card-actions justify-end">
+                <div className="card-title overflow-ellipsis">{domain.slice(0, 25)}</div>
+                <div className="flex flex-row items-center justify-center space-y-1 w-full">
+                    <div className="flex-1/2">
+                        <div className="text-lg">{sku?.price} <span className="text-sm">$/mo</span></div>
+                    </div>
+                    <div className="flex-1/2">
+                        <div className="text-md">{sku?.sku}</div>
+                    </div>
+                </div>
+                <div className="flex flex-row items-center justify-center space-y-1 w-full">
+                    <div className="flex-1/2">
+                        <div className="badge badge-success badge-sm">{vm.subscription_active ? "Active" : "Inactive"}</div>
+                    </div>
+                    <div className="flex-1/2">
+                        {vm.status === "a" ? (
+                            <div className="badge badge-success badge-sm">Online</div>
+                        ) : (
+                            <div className="badge badge-error badge-sm">Error</div>
+                        )}
+                    </div>
+                </div>
+                <div>
+                    IPv4: Not Implemented Yet
+                </div>
+                <div>
+                    IPv6: Not Implemented Yet
+                </div>
+                <div className="card-actions w-full justify-center mt-auto">
                     <Link href={`/dashboard/vm/${vm.id}`} className="btn btn-primary">manage</Link>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
