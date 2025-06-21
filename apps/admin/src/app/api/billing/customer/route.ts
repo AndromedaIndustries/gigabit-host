cb274061aec87c5bac601f10c5eadb138eff3294
Diana <dmoore@xn4p4lm.com>
2025-06-21 20:48:35 +0000

    Creating initial billing API structure

---
+++ apps/admin/src/app/api/billing/customer/route.ts
@@ -0,0 +1,27 @@
+import { getStripe } from "@/utils/stripe/stripe";
+import { createClient } from "@/utils/supabase/server";
+import { prisma } from "database";
+
+
+export async function GET() {
+    const authClient = await createClient();
+    const user = await authClient.auth.getUser();
+
+    if (!user) {
+        throw new Error("No user found");
+    }
+
+    const user_mapping = prisma.third_Party_User_Mapping.findUnique({
+        where: {
+            id: user.data.user?.id
+        }
+    })
+
+    if (!user_mapping) {
+        throw new Error("No User Mapping Found")
+    }
+
+    const stripe = getStripe();
+
+
+}
