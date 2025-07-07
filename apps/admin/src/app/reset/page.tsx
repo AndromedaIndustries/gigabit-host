37473c30ebd19c811f587522f09226d5b877ccbc
Diana <dmoore@xn4p4lm.com>
2025-07-07 03:39:07 +0000

    Fixing issue with password reset

--- apps/admin/src/app/reset/page.tsx
+++ apps/admin/src/app/reset/page.tsx
@@ -1,7 +1,19 @@
 import { UpdatePasswordForm } from '@/components/auth/resetPassword';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 
 export default async function Page() {
+    const supabase = await createClient();
+
+    const userResponse = await supabase.auth.getUser();
+
+    const user = userResponse.data.user;
+
+    if (user === null) {
+        redirect("/dashboard/login")
+    }
+
     return (
         <div className="flex items-center justify-center h-screen bg-base-100">
             <UpdatePasswordForm />
