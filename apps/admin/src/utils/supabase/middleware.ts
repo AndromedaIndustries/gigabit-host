219eab494ba5f3e1627dae718ebf0ea258da23d3
Diana <dmoore@xn4p4lm.com>
2025-09-08 02:25:58 +0000

    updating middleware to allow access to /invite

--- apps/admin/src/utils/supabase/middleware.ts
+++ apps/admin/src/utils/supabase/middleware.ts
@@ -47,7 +47,8 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/signup") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/dashboard/invite")
+    !request.nextUrl.pathname.startsWith("/update") &&
+    !request.nextUrl.pathname.startsWith("/invite")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
